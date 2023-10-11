8d1f17161adee7e5bf45be9ee6b517352462a5de
Razan <razaan.alaa@hotmail.com>
2023-10-11 03:19:24 -0700

    tries

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
---
+++ putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ *
+ * return: 0
+ */
+int char(char cha, int)
+{
+	return (0);
+}	
