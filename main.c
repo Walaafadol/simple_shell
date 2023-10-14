a2b6b3da0cba80eb55ea92291d93bc5ddef19da3
Walaafadol <walaafadul7@gmail.com>
2023-10-14 14:28:30 -0700

    ll

--- main.c
+++ main.c
@@ -4,39 +4,54 @@
  *
  * Description: to run a simple shell program
  * @argc: argument count
- * @argv: argument value
+ * @env: argument value
  * Return: void
  */
-int main(int argc, char **argv, char *env)
+int main(int argc, char **env)
 {
 (void)argc;
-char *prompt = "(Shell)$ ", *buffer = NULL;
+char *prompt = "(Shell)$ ";
+char *buffer = NULL;
+char *argv[20];
 size_t bufersize = 0;
 ssize_t numb_ch;
 pid_t child_id;
+int i, stutus;
 while (1)
 {
+if (isatty(0))
 _printstring(prompt);
 numb_ch = getline(&buffer, &bufersize, stdin);
 if (numb_ch == -1)
 {
-_printstring("shell stopped.....");
-return (-1);
+free(buffer);
+exit (0);
 }
+i = 0;
+while (buffer[i]) 
+{
+if (buffer[i] == '\n')
+buffer[i] = 0;
+i++;
+}
+argv[0] = _strdup(buffer);
 child_id = fork();
 if (child_id < 0)
 {
 _printstring("fork failed");
-return (-1);
+free(buffer);
+exit (0);
 }
 else if (child_id == 0)
 {
-execve(buffer[0], argv, env);
+if (execve(argv[0], argv, env) == -1)
+_printstring("command doesn't exit ..\n");
 }
-
+else
+wait(&stutus);
 _printstring(buffer);
 }
 
 free(buffer);
-	return (0);
+return (0);
 }
--- main.h
+++ main.h
@@ -5,9 +5,13 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 char *_strncat(char *dest, const char *src);
 int _putchar(char c);
 int _printstring(char *str);
+char *_strdup(const char *str);
+
 
 #endif
