17cd1b669e4cd6731384aee670039d3d4b3f43dc
Walaafadol <walaafadul7@gmail.com>
2023-10-16 14:46:05 -0700

    ll

--- main.c
+++ main.c
@@ -9,15 +9,9 @@
 int main(int argc, char **env)
 {
 (void)argc;
-<<<<<<< HEAD
 char *prompt ="(Shell)$ ";
 char *buffer = NULL, *path;
 char *argv[20], *delim = " \n";
-=======
-char *prompt = "(Shell)$";
-char *buffer = NULL;
-char *argv[20], *delim = "\n";
->>>>>>> 4c0990abdb7e56f8b57eab6073f8b679275040ab
 size_t bufersize = 0;
 ssize_t numb_ch;
 pid_t child_id;
@@ -41,7 +35,6 @@ j = 0;
 argv[j] = strtok(buffer, delim);
 while (argv[j])
 {
-<<<<<<< HEAD
 argv[++j] = strtok(NULL, delim);
 }
 argv[j] = NULL;
@@ -51,9 +44,6 @@ if (path == NULL)
 _printstring("command not found \n");
 continue;
 }
-=======
-argv[++j] = strtok(NULL, delim); }
->>>>>>> 4c0990abdb7e56f8b57eab6073f8b679275040ab
 child_id = fork();
 if (child_id < 0)
 {
@@ -62,15 +52,13 @@ free(buffer);
 exit(0); }
 else if (child_id == 0)
 {
-<<<<<<< HEAD
 if (execve(path, argv, env) == -1)
 _printstring("command doesn't exit ..\n");
 }
-=======
-if (execve(argv[0], argv, env) == -1)
-_printstring("command doesn't exit ..\n"); }
->>>>>>> 4c0990abdb7e56f8b57eab6073f8b679275040ab
 else
-wait(&stutus); }
+wait(&stutus);
+}
 free(buffer);
-return (0); }
+return (0); 
+}
+
--- main.h
+++ main.h
@@ -9,22 +9,15 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 4c0990abdb7e56f8b57eab6073f8b679275040ab
 char *_strncat(char *dest, const char *src);
 int _putchar(char c);
 int _printstring(char *str);
 char *_strdup(const char *str);
-<<<<<<< HEAD
 char *location(char *path, char *args);
 char *gets_loc(char *arg);
 
-=======
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 char *getEnv(const char *variable);
->>>>>>> 4c0990abdb7e56f8b57eab6073f8b679275040ab
-
 #endif
