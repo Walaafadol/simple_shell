6701ad26a022e7d07b2781d3b561cfe9794240d1
Razan <razaan.alaa@hotmail.com>
2023-10-20 13:59:07 -0700

    lian

--- main.h
+++ main.h
@@ -8,15 +8,12 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-<<<<<<< HEAD
 #include <sys/stat.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stddef.h>
-=======
 #include <fcntl.h>
 #include <dirent.h>
->>>>>>> 643b2c5b0e5b36029e88bfbbd02259e2001516d3
 
 
 #define DELIM " \t\n"
@@ -31,11 +28,10 @@ char *location(char *path, char *args);
 char *gets_loc(char *arg);
 int _execute(char **command, char **arg, int idx);
 char *_getspath(char *command);
-
+int _atoi(char *s);
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 char *getEnv(const char *variable);
-<<<<<<< HEAD
 char *_strcat(char *s1, char *s2);
 char *_strcpy(char *s1, char *s2);
 void freearray(char **arr);
@@ -45,10 +41,5 @@ void printerror(char *name, char *cmd, int dx);
 char *_itoa(int s);
 void reverse_st(char *str, int len);
 
-
-
-
-=======
->>>>>>> 643b2c5b0e5b36029e88bfbbd02259e2001516d3
 
 #endif
